85efce245e1c7d854ff0f53c4733e27d3cd90c9b
telkar-rg <telkar-rg@gmx.net>
2025-01-31 16:54:26 +0100

    attempt 1

--- TransmogShardTracker/TransmogShardTracker.lua
+++ TransmogShardTracker/TransmogShardTracker.lua
@@ -1,3 +1,31 @@
 local ADDON_NAME, ADDON_TABLE = ...
 
 
+local Locale = GetLocale()
+
+
+local TOKENS_SHARDS_OF_ILLUSION = "Shards of Illusion|n|cffffffff%d/%d|r"
+local TOKENS_SHARDS_OF_ILLUSION_TOOLTIP = "Click to update"
+if Locale == "deDE" then
+	TOKENS_SHARDS_OF_ILLUSION = "Splitter der Illusion|n|cffffffff%d/%d|r"
+	TOKENS_SHARDS_OF_ILLUSION_TOOLTIP = "Zum Updaten klicken"
+end
+
+
+function TransmogShardTracker_OnLoad(self)
+	self.text:SetFormattedText(TOKENS_SHARDS_OF_ILLUSION, 0,0)
+	self.tooltip = TOKENS_SHARDS_OF_ILLUSION_TOOLTIP
+end
+
+
+function TransmogShardTracker_OnUpdate(self, elapsed)
+	-- self.tooltip = TOKENS_SHARDS_OF_ILLUSION_TOOLTIP
+	
+	self.text:SetFormattedText(TOKENS_SHARDS_OF_ILLUSION, 0,0)
+end
+
+
+function TransmogShardTracker_OnClick(self)
+	print("TransmogShardTracker_OnClick")
+end
+
